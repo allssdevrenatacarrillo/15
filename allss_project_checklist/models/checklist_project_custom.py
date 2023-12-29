168ce0b624546544fa78d3f2f29cbca60f0f1e6f
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2023-12-29 12:26:47 -0300

    teste 17

--- allss_project_checklist/models/checklist_project_custom.py
+++ allss_project_checklist/models/checklist_project_custom.py
@@ -9,7 +9,7 @@ class ProjectProjectCustom(models.Model):
     # model_a_id = fields.Many2one('account.move', string='Model A')
     # model_a_id = fields.Many2one('account.move', related='sale_line_id.order_id.invoice_ids', string='Model A')
     # model_a_id = fields.Many2one(related='sale_line_id.order_id.invoice_ids', string='Model A')
-    model_a_id = fields.Selection(related='sale_line_id.order_id.invoice_ids.id', string='Model A')
+    model_a_id = fields.Many2one('project.project', related='sale_line_id.order_id.invoice_ids.id', string='Model A')
     # allss_status_pagamento = fields.Selection(related='model_a_id.payment_state', string="Status Pagamento Auditoria", store=True)
     allss_status_pagamento = fields.Selection(related='sale_line_id.order_id.invoice_ids.payment_state', string="Status Pagamento Auditoria", store=True)
     # allss_status_pagamento = fields.Selection(related='model_a_id', string="Status Pagamento Auditoria", store=True)
