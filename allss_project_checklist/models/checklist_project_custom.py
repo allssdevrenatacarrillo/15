5c96141c2ab4af7f02093edc145653ca05722fe5
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2023-12-29 13:46:15 -0300

    teste 30

--- allss_project_checklist/models/checklist_project_custom.py
+++ allss_project_checklist/models/checklist_project_custom.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 class ProjectProjectCustom(models.Model):
     _inherit = 'project.project'
 
-    model_a_id = fields.Many2one('account.move', string='Model A')
+    # model_a_id = fields.Many2one('account.move', string='Model A')
     # allss_status_pagamento = fields.Selection(related='model_a_id.payment_state', string="Status Pagamento Auditoria", store=True)
     # allss_status_pagamento = fields.Selection(related='sale_line_id.order_id.invoice_ids.payment_state', string="Status Pagamento Auditoria", store=True)
     # cond_pedido_field = fields.Char(string='Nome', compute="_get_pedido", readonly=True)
@@ -19,4 +19,6 @@ class ProjectProjectCustom(models.Model):
 
     # _logger.warning(f'_____>STATUS PAGAMENTO AUDITORIA: {order_id.id} <________')
 
-    nome_fatura = fields.Char(related="sale_line_id.order_id.invoice_ids.name", string="nome fatura")
+    nome_fatura = fields.Many2one('project.project', related="sale_line_id.order_id.invoice_ids.id", string='nome fatura')
+    # nome_fatura = fields.Char(related="sale_line_id.order_id.invoice_ids.id", string="nome fatura")
+    allss_status_pagamento = fields.Selection(related='nome_fatura.payment_state', string="Status Pagamento Auditoria", store=True)
