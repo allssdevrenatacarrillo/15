5a2f381ee223f61a38a425a14f1c3a357b028fe0
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2023-12-29 14:20:07 -0300

    teste 37

--- allss_project_checklist/models/checklist_project_custom.py
+++ allss_project_checklist/models/checklist_project_custom.py
@@ -17,14 +17,14 @@ class ProjectProjectCustom(models.Model):
     #     return cond_pedido
 
 
-    # id_fatura = fields.Char(related="sale_line_id.order_id.invoice_ids.id", string="nome fatura")
+    id_fatura = fields.Char(related="sale_line_id.order_id.invoice_ids.id", string="nome fatura")
     # id_fatura_dois = fields.Many2one('account.move', compute='set_account_move_id', string='nome fatura')
     # nome_fatura = fields.Char(related="sale_line_id.order_id.invoice_ids.id", string="nome fatura")
     # nome_fatura = {sale_line_id.order_id.invoice_ids.id}
     # allss_status_pagamento = fields.Selection(related='id_fatura_dois.payment_state', string="Status Pagamento Auditoria", store=True)
-    payment_state = fields.Char(related="sale_line_id.order_id.invoice_ids.payment_state", string="nome fatura")
-    allss_status_pagamento = fields.Selection(related='payment_state.payment_state', string="Status Pagamento Auditoria", store=True)
+    payment_state = None
+    allss_status_pagamento = fields.Selection(related='payment_state.payment_state', compute='set_account_move_id', string="Status Pagamento Auditoria", store=True)
 
-    # def set_account_move_id(self):    
-    #     id_account = self.env['account.move'].search([('id','=', self.id_fatura)])
-    #     self.id_fatura_dois = id_account
+    def set_account_move_id(self):    
+        id_account = self.env['account.move'].search([('id','=', self.id_fatura)])
+        self.payment_state = id_account
